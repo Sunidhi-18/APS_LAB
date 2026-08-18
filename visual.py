91d439e123fab205d3eff05ee842a35b0e4b3ac9
Sunidhi <152707068+Sunidhi-18@users.noreply.github.com>
2026-08-18 12:44:33 +0530

    Delete visual.py

--- visual.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plot_2d_data(x,y,title="linear data"):
-    plt.scatter(x[:,0],x[:,1],c=y)
-    plt.title("title")
-    plt.xlabel("x1")
-    plt.ylabel("x2")
-    plt.show()
