1ac936f6fcba9728d516e52098588d936a9c6323
Sunidhi <152707068+Sunidhi-18@users.noreply.github.com>
2026-08-18 12:44:23 +0530

    Delete task1.py

--- task1.py
+++
@@ -1,9 +0,0 @@
-from data_gen import generate_nonlinear_data
-from visual import plot_2d_data
-
-def main():
-    x,y = generate_nonlinear_data()
-    plot_2d_data(x,y,title="nonlinearly separable data")
-
-if __name__ == "__main__":
-    main()
