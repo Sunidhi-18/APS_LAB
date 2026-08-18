13279aa8b326542006bb165ffbeb080363fc78e1
Sunidhi <152707068+Sunidhi-18@users.noreply.github.com>
2026-08-18 12:44:14 +0530

    Delete task0.py

--- task0.py
+++
@@ -1,9 +0,0 @@
-from data_gen import generate_linear_data
-from visual import plot_2d_data
-
-def main():
-    x,y = generate_linear_data()
-    plot_2d_data(x,y,title="linearly separable data")
-
-if __name__ == "__main__":
-    main()
